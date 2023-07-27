084c66ac66bf6a9332fea1be6cf1dc49d00e69e6
iknowimhere <sunilkumarsk4078@gmail.com>
2023-07-27 09:12:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CurrentLocation from "./CurrentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+   
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,9 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+//https://openweathermap.org/city/1264527
+module.exports = {
+    key: "c9402870322bbd2f26a0608ee2523a33",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+  
+  //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//   https://api.openweathermap.org/data/2.5/weather?q={city%20name}&units=metric&APPID={API%20key}
